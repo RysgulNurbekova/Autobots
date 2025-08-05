6afb7db74f2d45e03f751f2a93aba3e8a5c5e045
Rysgul <rysyanur123@gmail.com>
2025-08-05 20:51:15 +0600

    Version 3

--- src/main/java/Reset.java
+++ src/main/java/Reset.java
@@ -3,5 +3,6 @@ public class Reset {
         System.out.println("Reset-demo");
         System.out.println("Version 1");
         System.out.println("Version 2");
+        System.out.println("Version 3");
     }
 }
