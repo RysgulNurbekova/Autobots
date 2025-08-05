3ed9d54100bb60ecd071061af7b9cdc5b14ffa38
Rysgul <rysyanur123@gmail.com>
2025-08-05 20:50:48 +0600

    Version 2

--- src/main/java/Reset.java
+++ src/main/java/Reset.java
@@ -2,5 +2,6 @@ public class Reset {
     public static void main(String[] args) {
         System.out.println("Reset-demo");
         System.out.println("Version 1");
+        System.out.println("Version 2");
     }
 }
