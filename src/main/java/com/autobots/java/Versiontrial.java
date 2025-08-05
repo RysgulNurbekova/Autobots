12f2dfa4f6530ad2804e2a18b2c3bec834a30069
Rysgul <rysyanur123@gmail.com>
2025-08-05 20:03:24 +0600

    Revert "version 1.0.2"
    This reverts commit 2c45dd104b4c9a57c74413c5fcab9eca53ba94ca.

--- src/main/java/com/autobots/java/Versiontrial.java
+++ src/main/java/com/autobots/java/Versiontrial.java
@@ -4,6 +4,5 @@ public class Versiontrial {
     public static void main(String[] args) {
         System.out.println("version1.0.0");
         System.out.println("version 1.0.1");
-        System.out.println("version 1.0.2");
     }
 }
